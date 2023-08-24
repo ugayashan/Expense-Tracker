210ff333ae9aa3f9a548d3522c96a45089527c00
Kanishka Mallawaarachchi <mallawarachchikanishka@gmail.com>
2023-08-25 05:16:04 +0800

    category firbase push
    category data push to firebase push

--- Expense-Tracker/Core/Category/Service/CategoryService.swift
+++ Expense-Tracker/Core/Category/Service/CategoryService.swift
@@ -16,25 +16,51 @@ class CategoryService{
     static let shared = CategoryService()
     
     func getAllCategories(user:String)async throws{
-//        do{
-//
-//        }catch{
-//
-//        }
+        incomeCatagories.removeAll()
+        expenseCatagories.removeAll()
+        
+        let db  = Firestore.firestore()
+        let ref = db.collection("Categories").whereField("userid", isEqualTo: user)
+        ref.getDocuments{
+            snapshot, error in
+            guard error == nil else{
+                print(error!.localizedDescription)
+                return
+            }
+            
+            if let snapshot = snapshot{
+                for documents in snapshot.documents{
+                    let data = documents.data()
+                    let catid = data["catid"] as? String ?? ""
+                    let catname = data["catName"] as? String ?? ""
+                    let catimage = data["catImage"] as? String ?? ""
+                    let catType = data["catType"] as? String ?? ""
+                    let userid = data["userid"] as? String ?? ""
+                    let cat = Category(catId: catid, catName: catname, catType: catType, catImage: catimage,userid: userid)
+                    
+                    if(catType == "Income"){
+                        self.incomeCatagories.append(cat)
+                    }else{
+                        self.expenseCatagories.append(cat)
+                    }
+                    
+                }
+            }
+        }
+        
     }
     
     func saveCategory(userId:String, catId:String, catType:String, catName:String, catImage:String) async throws{
 
         let  db = Firestore.firestore()
         let ref = db.collection("category").document()
-        
-//        let catId: String
-//        let catName:String
-//        let catType:String
-//        let catImage:String
-//        var userid:String?
-//
-        ref.setData(["catid":catId, "catName": catName, "catType": catType, "catImage": catImage, "userid": userId]){
+
+        ref.setData(["catid":catId,
+                     "catName": catName,
+                     "catType": catType,
+                     "catImage": catImage,
+                     "userid": userId
+                    ]){
             error in if let error = error{
                 print(error.localizedDescription)
             }
--- Expense-Tracker/Core/Category/ViewModel/AddCategoryListViewModel.swift
+++ Expense-Tracker/Core/Category/ViewModel/AddCategoryListViewModel.swift
@@ -10,15 +10,15 @@ import Foundation
 
 class AddCategoryListViewModel: ObservableObject{
     
-    @Published var userId = ""
+    @Published var userId =  ""
     @Published var catId = ""
-    @Published var catType = ""
+    @Published var catType = "Income"
     @Published var catName = ""
     @Published var catImage = ""
     
     @MainActor
     func createCategory() async throws{
-        print("DEBUG: \(userId) here")
+//        print("DEBUG: \(userId) \(catId) \(catType) \(catName) \(catImage)")
         try await CategoryService.shared.saveCategory(userId: userId, catId: catId, catType: catType, catName: catName, catImage: catImage)
     }
     
